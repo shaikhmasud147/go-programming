960d25a648d9633360b96b8664e0647dc4bc995e
masud <Masud Shaikh>
2020-12-13 14:23:24 +0530

    Exrcise go

---
+++ Level_7/go_1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main()  {
+	x  := 20
+
+	fmt.Println(x)
+	fmt.Println(&x)
+}
---
+++ Level_7/go_2.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type person struct {
+	fname string
+	lname string
+}
+
+func main()  {
+	x := person{
+		fname: "Masud",
+		lname: "Shaikh",
+	}
+	fmt.Println(x)
+	changeMe(&x)
+	fmt.Println(x)
+}
+
+func changeMe(p *person)  {
+	p.fname = "Rahul"
+	p.lname = "Sawant"
+}
